0a86c9532a282a58c86b5beba376b1ffd950adff
Anthony Petrosino <apetrosino789@gmail.com>
2025-01-01 20:59:09 -0500

    Resume Update

--- website.js
+++ website.js
@@ -1,15 +1,8 @@
 var resumeButton = document.getElementById("resume");
-var pdfViewer = document.getElementById("pdfViewer");
 
 document.getElementById("resume").addEventListener("click", function () {
-    const resumePdf = document.getElementById("resumePdf");
-    if (resumePdf.style.display === "none") {
-        resumePdf.style.display = "block";
-        this.textContent = "Hide Resume"; // Update button text
-    } else {
-        resumePdf.style.display = "none";
-        this.textContent = "Show Resume";
-    }
+    window.open("static/" + encodeURIComponent("Petrosino, Anthony Resume.pdf"), "_blank");
+    this.textContent = "Please enjoy reviewing my resume, feel free to contact me with more questions.";
 });
 
 $(document).ready(function() {
@@ -41,27 +34,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Add 'active' class to the current menu item
     document.getElementById(page.split(".")[0]).classList.add('active');
-});
-
-window.addEventListener("DOMContentLoaded", () => {
-    const shapes = document.querySelectorAll('.background-shape');
-
-    shapes.forEach(shape => {
-        // Set a random position within the viewport
-        const randomTop = Math.random() * 100; // Random vertical position percentage
-        const randomLeft = Math.random() * 100; // Random horizontal position percentage
-        const randomSize = Math.random() * 50 + 50; // Random size between 50px and 100px
-
-        // Set a random delay for the animation
-        const randomDelay = Math.random() * 5; // Random delay between 0s and 5s
-
-        // Apply styles to each shape
-        shape.style.top = `${randomTop}%`;
-        shape.style.left = `${randomLeft}%`;
-        shape.style.width = `${randomSize}px`;
-        shape.style.height = `${randomSize}px`;
-        shape.style.animationDelay = `${randomDelay}s`; // Apply random delay for animation
-    });
 });
 
 // Smooth Scrolling
